fa398be9f6cb4d458570ed42f0c3becf41ada894
Store-ios <119080464+Store-ios@users.noreply.github.com>
2023-04-10 23:17:41 +0300

    Update submit.php

--- complete/submit.php
+++ complete/submit.php
@@ -1,6 +1,6 @@
 <?php
 	$a = $_POST['udid'] . "\n" . $_POST['name'] . "\n" . $_POST['email'] . "\n" . $_POST['product'];
-	mail("YOU@YOUR-DOMAIN.c-o-m", "UDID", $a);
+	mail("zhanyar.store@gmail.com", "UDID", $a);
 	header('Location: finished.html');
 ?>
 	
